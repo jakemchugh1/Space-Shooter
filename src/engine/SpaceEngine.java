2616f1b920d517c47ce054912c0b10a9c8a7f3e8
jakemchugh1 <jakemchugh1@gmail.com>
2019-09-18 12:54:09 -0700

    Fullscreen
    game is now full screen. Now we have an issue with the mouse.

--- src/engine/SpaceEngine.java
+++ src/engine/SpaceEngine.java
@@ -2,6 +2,8 @@ package engine;
 
 import audio.AudioManager;
 import manager.GameManager;
+import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import static utilities.Artist.*;
 import static org.lwjgl.opengl.GL11.*;
@@ -14,11 +16,14 @@ public class SpaceEngine {
 
 
     public SpaceEngine()  {
+
         BeginSession();
 
         exitGame = false;
 
         GameManager manager = new GameManager();
+
+
 
         while (!Display.isCloseRequested() && !exitGame) {
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
